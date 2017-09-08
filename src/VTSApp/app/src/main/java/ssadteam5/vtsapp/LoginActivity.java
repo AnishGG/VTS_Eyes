11b6b937c4fa05fb320cfc7602545a8f19a08912
Ravsimar Singh Sodhi <ravsimar.sodhi@research.iiit.ac.in>
2017-09-08 15:10:12 +0530

    added error messages in loginActivity

--- src/VTSApp/app/src/main/java/ssadteam5/vtsapp/LoginActivity.java
+++ src/VTSApp/app/src/main/java/ssadteam5/vtsapp/LoginActivity.java
@@ -4,28 +4,16 @@ import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -34,35 +22,29 @@ import android.widget.TextView;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
-
-import java.io.BufferedReader;
-import java.io.DataOutput;
-import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.Manifest.permission.READ_CONTACTS;
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 /**
  * A login screen that offers login via email, password and tenant ID.
  */
-public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor>
+public class LoginActivity extends AppCompatActivity
 {
-    String status = "FAILURE";
-    //The variable status determines whether the login is successful or not.
+    /**
+     *  These variables store the login response
+     */
+    private String status;
+    private String token;
+    private String errorCode;
+    private String errorMessage;
+    private String[] menu;
 
-    private static final int REQUEST_READ_CONTACTS = 0;
+    //The variable status determines whether the login is successful or not.
 
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -70,7 +52,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private UserLoginTask mAuthTask = null;
 
     // UI references.
-    private AutoCompleteTextView mEmailView;
+    private EditText mEmailView;
     private EditText mPasswordView;
     private EditText mTenantIdView;
     private View mProgressView;
@@ -82,12 +64,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         // Set up the login form.
-        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
-        populateAutoComplete();
+        mEmailView = (EditText) findViewById(R.id.email);
 
         mPasswordView = (EditText) findViewById(R.id.password);
         mTenantIdView = (EditText) findViewById(R.id.tenantId);
-        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener()
+        mTenantIdView.setOnEditorActionListener(new TextView.OnEditorActionListener()
         {
             @Override
             public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent)
@@ -110,60 +91,10 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             }
         });
 
-
         mLoginFormView = findViewById(R.id.login_form);
         mProgressView = findViewById(R.id.login_progress);
     }
 
-    private void populateAutoComplete()
-    {
-        if (!mayRequestContacts())
-        {
-            return;
-        }
-
-        getLoaderManager().initLoader(0, null, this);
-    }
-
-    private boolean mayRequestContacts()
-    {
-        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
-            return true;
-        }
-        if (checkSelfPermission(READ_CONTACTS) == PackageManager.PERMISSION_GRANTED) {
-            return true;
-        }
-        if (shouldShowRequestPermissionRationale(READ_CONTACTS)) {
-            Snackbar.make(mEmailView, R.string.permission_rationale, Snackbar.LENGTH_INDEFINITE)
-                    .setAction(android.R.string.ok, new View.OnClickListener()
-                    {
-                        @Override
-                        @TargetApi(Build.VERSION_CODES.M)
-                        public void onClick(View v)
-                        {
-                            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-                        }
-                    });
-        } else {
-            requestPermissions(new String[]{READ_CONTACTS}, REQUEST_READ_CONTACTS);
-        }
-        return false;
-    }
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults)
-    {
-        if (requestCode == REQUEST_READ_CONTACTS)
-        {
-            if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                populateAutoComplete();
-            }
-        }
-    }
 
 
     /**
@@ -207,7 +138,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
         if (TextUtils.isEmpty(tenantId))
         {
-            mTenantIdView.setError("This field is required");
+            mTenantIdView.setError(getString(R.string.error_field_required));
             focusView = mTenantIdView;
             cancel = true;
         }
@@ -280,7 +211,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                     mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
                 }
             });
-        } else {
+        }
+        else
+        {
             // The ViewPropertyAnimator APIs are not available, so simply show
             // and hide the relevant UI components.
             mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
@@ -288,64 +221,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         }
     }
 
-    @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle)
-    {
-        return new CursorLoader(this,
-                // Retrieve data rows for the device user's 'profile' contact.
-                Uri.withAppendedPath(ContactsContract.Profile.CONTENT_URI,
-                        ContactsContract.Contacts.Data.CONTENT_DIRECTORY), ProfileQuery.PROJECTION,
-
-                // Select only email addresses.
-                ContactsContract.Contacts.Data.MIMETYPE +
-                        " = ?", new String[]{ContactsContract.CommonDataKinds.Email
-                .CONTENT_ITEM_TYPE},
-
-                // Show primary email addresses first. Note that there won't be
-                // a primary email address if the user hasn't specified one.
-                ContactsContract.Contacts.Data.IS_PRIMARY + " DESC");
-    }
-
-    @Override
-    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor)
-    {
-        List<String> emails = new ArrayList<>();
-        cursor.moveToFirst();
-        while (!cursor.isAfterLast()) {
-            emails.add(cursor.getString(ProfileQuery.ADDRESS));
-            cursor.moveToNext();
-        }
-
-        addEmailsToAutoComplete(emails);
-    }
-
-    @Override
-    public void onLoaderReset(Loader<Cursor> cursorLoader)
-    {
-
-    }
-
-    private void addEmailsToAutoComplete(List<String> emailAddressCollection)
-    {
-        //Create adapter to tell the AutoCompleteTextView what to show in its dropdown list.
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<>(LoginActivity.this,
-                        android.R.layout.simple_dropdown_item_1line, emailAddressCollection);
-
-        mEmailView.setAdapter(adapter);
-    }
-
-
-    private interface ProfileQuery
-    {
-        String[] PROJECTION = {
-                ContactsContract.CommonDataKinds.Email.ADDRESS,
-                ContactsContract.CommonDataKinds.Email.IS_PRIMARY,
-        };
-
-        int ADDRESS = 0;
-        int IS_PRIMARY = 1;
-    }
 
     private void launchMap()
     {
@@ -373,7 +248,6 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         @Override
         protected Boolean doInBackground(Void... params)
         {
-            // TODO: attempt authentication against a network service.
 
             HttpURLConnection conn;
             try {
@@ -385,8 +259,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
                 jsonObject.put("menu", jsonArray);
                 jsonObject.put("password", mPassword);
                 jsonObject.put("tenantId", mTenantId);
-                //Log.d("myTag0","Testing");
-                URL url = new URL("http://eyedentifyapps.com:8080/login/");
+                URL url = new URL(getString(R.string.login_controller));
                 conn = (HttpURLConnection) url.openConnection();
                 conn.setRequestMethod("POST");
                 conn.setRequestProperty("Accept","*/*");
@@ -409,8 +282,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
                 JSONObject resp = new JSONObject(response);
                 status = resp.get("status").toString();
-
-                //Log.d("myTag",obj.get("status").toString());
+                token = resp.get("token").toString();
+                errorCode = resp.get("errorCode").toString();
+                errorMessage = resp.get("errorMessage").toString();
+                //JSONArray menu = resp.get("menu");
+                //menu.to
                 //Thread.sleep(2000);
             }
 
@@ -440,13 +316,35 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             mAuthTask = null;
             showProgress(false);
 
-            if (success) {
+            if (success)
+            {
                 if(status.equals("SUCCESS"))
                 {
                     launchMap();
                 }
+                else if(status.equals("FAILURE"))
+                {
+                    if(errorCode.equals("3116"))
+                    {
+                        mEmailView.setError(errorMessage);
+                        mEmailView.requestFocus();
+                    }
+                    else if(errorCode.equals("4103"))
+                    {
+                        mPasswordView.setError(errorMessage);
+                        mPasswordView.requestFocus();
+                    }
+                    else if(errorCode.equals("4104"))
+                    {
+                        mTenantIdView.setError(errorMessage);
+                        mTenantIdView.requestFocus();
+                    }
+                }
+
                 //finish();
-            } else {
+            }
+            else
+            {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
                 mPasswordView.requestFocus();
             }
