96476e4f742b84f5df1236a4e52ed28e4d735036
daksh.lalwani <daksh.lalwani@students.iiit.ac.in>
2017-10-30 03:10:02 +0530

    reports algo coded;tables displaying data

--- src/VTSApp/app/src/main/java/ssadteam5/vtsapp/IdleReport.java
+++ src/VTSApp/app/src/main/java/ssadteam5/vtsapp/IdleReport.java
@@ -3,6 +3,7 @@ package ssadteam5.vtsapp;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +16,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Objects;
 
 
@@ -29,9 +33,11 @@ public class IdleReport extends Fragment {
         if (bundle != null) {
             try {
                 JSONArray jsonArray = new JSONArray(getArguments().getString("resp"));
-                int counti = 1;
-                int k = 0, l = 0;
-                for (int i = 0; i < jsonArray.length(); i++) {
+                int counti = 0;
+                int k = 1, flagi = 0, i;
+                long millis, second, minute, hour;
+                String engst, Starttime = "", Endtime = "", locstart = "0.000000" + "," + "0.000000", locend = "0.000000" + "," + "0.000000", time1, time2, timedur;
+                for (i = 0; i < jsonArray.length(); i++) {
                     TableRow tr1 = new TableRow(getActivity());
                     TextView tv1 = new TextView(getActivity());
                     TextView tv2 = new TextView(getActivity());
@@ -40,14 +46,42 @@ public class IdleReport extends Fragment {
                     TextView tv5 = new TextView(getActivity());
                     TextView tv6 = new TextView(getActivity());
                     JSONObject ob = jsonArray.getJSONObject(i);
-                    String engst = ob.getString("EngineStatus");
-                    if (Objects.equals(engst, "ON") && counti == 1) {
+                    engst = ob.getString("EngineStatus");
+                    if (Objects.equals(engst, "OFF")) {
+                        counti = 1;
+                        if (flagi == 0) {
+                            Starttime = ob.getString("GPSTimestamp");
+                            locstart = ob.getString("Latitude") + "," + ob.getString("Longitude");
+                            flagi = 1;
+                        }
+                        Log.d("after", "inoutif");
+                    } else if (Objects.equals(engst, "ON") && counti == 1) {
+                        counti = 0;
+                        flagi = 0;
+                        JSONObject obj1 = jsonArray.getJSONObject(i - 1);
+                        Endtime = obj1.getString("GPSTimestamp");
+                        time1 = Starttime.substring(Starttime.indexOf("T")+1, Starttime.indexOf("Z"));
+                        time2 = Endtime.substring(Endtime.indexOf("T")+1, Endtime.indexOf("Z"));
+                        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                        Date date1 = format.parse(time1);
+                        Date date2 = format.parse(time2);
+                        millis = date2.getTime() - date1.getTime();
+                        second = (millis / 1000) % 60;
+                        minute = (millis / (1000 * 60)) % 60;
+                        hour = (millis / (1000 * 60 * 60)) % 24;
+                        timedur = String.format("%02d:%02d:%02d", hour, minute, second);
                         tv1.setText(String.valueOf(k));
                         tv2.setText(ob.getString("DeviceId"));
-                        tv3.setText(ob.getString("GPSTimestamp"));
-                        tv4.setText(ob.getString("GPSTimestamp"));
-                        tv5.setText(ob.getString("GPSTimestamp"));
-                        tv6.setText(ob.getString("GPSTimestamp"));
+                        tv3.setText(Starttime);
+                        tv4.setText(Endtime);
+                        tv5.setText(locstart);
+                        tv6.setText(timedur);
+                        tv1.setGravity(Gravity.CENTER_HORIZONTAL);
+                        tv2.setGravity(Gravity.CENTER_HORIZONTAL);
+                        tv3.setGravity(Gravity.CENTER_HORIZONTAL);
+                        tv4.setGravity(Gravity.CENTER_HORIZONTAL);
+                        tv5.setGravity(Gravity.CENTER_HORIZONTAL);
+                        tv6.setGravity(Gravity.CENTER_HORIZONTAL);
                         tr1.addView(tv1);
                         tr1.addView(tv2);
                         tr1.addView(tv3);
@@ -55,34 +89,52 @@ public class IdleReport extends Fragment {
                         tr1.addView(tv5);
                         tr1.addView(tv6);
                         list.addView(tr1);
-                        counti = 0;
                         k++;
-                    } else if (Objects.equals(engst, "OFF") && counti == 0) {
-                        JSONObject jo = jsonArray.getJSONObject(i);
-                        tv1.setText(String.valueOf(l));
-                        tv2.setText(jo.getString("DeviceId"));
-                        tv3.setText(jo.getString("GPSTimestamp"));
-                        tv4.setText(jo.getString("GPSTimestamp"));
-                        tv5.setText(jo.getString("GPSTimestamp"));
-                        tv6.setText(jo.getString("GPSTimestamp"));
-                        tr1.addView(tv1);
-                        tr1.addView(tv2);
-                        tr1.addView(tv3);
-                        tr1.addView(tv4);
-                        tr1.addView(tv5);
-                        tr1.addView(tv6);
-                        list.addView(tr1);
-                        counti = 1;
-                        l++;
                     }
-                    //                    Log.d("test", ob.toString());
-                    //                counter++;
-                    //                Log.d("counter", String.valueOf(counter));
                 }
-                //            if(k%2){
-                //
-                //            }
+                if(flagi == 1){
+                    TableRow tr1 = new TableRow(getActivity());
+                    TextView tv1 = new TextView(getActivity());
+                    TextView tv2 = new TextView(getActivity());
+                    TextView tv3 = new TextView(getActivity());
+                    TextView tv4 = new TextView(getActivity());
+                    TextView tv5 = new TextView(getActivity());
+                    TextView tv6 = new TextView(getActivity());
+                    JSONObject obj1 = jsonArray.getJSONObject(i - 1);
+                    Endtime = obj1.getString("GPSTimestamp");
+                    time1 = Starttime.substring(Starttime.indexOf("T")+1, Starttime.indexOf("Z"));
+                    time2 = Endtime.substring(Endtime.indexOf("T")+1, Endtime.indexOf("Z"));
+                    SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+                    Date date1 = format.parse(time1);
+                    Date date2 = format.parse(time2);
+                    millis = date2.getTime() - date1.getTime();
+                    second = (millis / 1000) % 60;
+                    minute = (millis / (1000 * 60)) % 60;
+                    hour = (millis / (1000 * 60 * 60)) % 24;
+                    timedur = String.format("%02d:%02d:%02d", hour, minute, second);
+                    tv1.setText(String.valueOf(k));
+                    tv2.setText(obj1.getString("DeviceId"));
+                    tv3.setText(Starttime);
+                    tv4.setText(Endtime);
+                    tv5.setText(locstart);
+                    tv6.setText(timedur);
+                    tv1.setGravity(Gravity.CENTER_HORIZONTAL);
+                    tv2.setGravity(Gravity.CENTER_HORIZONTAL);
+                    tv3.setGravity(Gravity.CENTER_HORIZONTAL);
+                    tv4.setGravity(Gravity.CENTER_HORIZONTAL);
+                    tv5.setGravity(Gravity.CENTER_HORIZONTAL);
+                    tv6.setGravity(Gravity.CENTER_HORIZONTAL);
+                    tr1.addView(tv1);
+                    tr1.addView(tv2);
+                    tr1.addView(tv3);
+                    tr1.addView(tv4);
+                    tr1.addView(tv5);
+                    tr1.addView(tv6);
+                    list.addView(tr1);
+                }
             } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (ParseException e) {
                 e.printStackTrace();
             }
         }
